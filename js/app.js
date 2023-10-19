56473206864bedf68dcda076d60bf6b7ebfa4cb3
ximenag0119 <ximenaga012005@gmail.com>
2023-10-19 15:16:54 -0500

    V4

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ const cargarTabla = () => {
         <td>${item.tipo}</td>
         <td>${item.tiempo}</td>
         <td>$${item.precio}</td>
-        <td><img src="${item.imagen}"></td>
+        <td><img src="${item.imagen}" style="max-width: 92px; max-height: 90px;"></td>
         <td>
         <div class="d-flex gap-2">
         <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
